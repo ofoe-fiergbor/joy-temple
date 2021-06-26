e7df1de1854c273d2661e93bf69fb38c3faedf3e
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2021-06-26 17:35:36 +0000

    form submit activation

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -14,7 +14,7 @@ function Contact() {
               </p>
             </div>
             <form
-              action="https://formsubmit.co/27f5522deab9f9bb86d42c38442de305 "
+              action="https://formsubmit.co/27f5522deab9f9bb86d42c38442de305   "
               method="POST"
               name="sentMessage"
               id="contactForm"
